dfc60ac2b66eba7c6329176dcfab56abc620081c
= <=>
2024-06-04 17:50:21 -0300

    ready

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-document.getElementById('search2').addEventListener('click', function() {
-    const searchInput = document.getElementById('search1').value;
-    if (searchInput) {
-        fetchMovies(searchInput);
-    }
-});
+document.getElementById('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const query = document.getElementById('search1').value;
+    searchMovies(query);
+  });
 
-async function fetchMovies(query) {
+async function searchMovies(query) {
     const apiKey = 'd661769b';  // 
     const url = `http://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
     try {
@@ -14,29 +13,59 @@ async function fetchMovies(query) {
         if (data.Response === "True") {
             displayMovies(data.Search);
         } else {
-            displayError(data.Error);
+            displayError([]);
         }
     } catch (error) {
-        displayError('Error fetching data');
+       
     }
 }
 
 function displayMovies(movies) {
     const moviesContainer = document.getElementById('peliculas');
     moviesContainer.innerHTML = '';
+  
     movies.forEach(movie => {
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('movie');
-        movieElement.innerHTML = `
-            <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'}" alt="${movie.Title}">
-            <h2>${movie.Title}</h2>
-            <p>${movie.Year}</p>
-        `;
-        moviesContainer.appendChild(movieElement);
+      const movieElement = document.createElement('div');
+      movieElement.classList.add('movie');
+      movieElement.innerHTML = `
+        <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'}" alt="${movie.Title}">
+        <h2>${movie.Title}</h2>
+        <p>${movie.Year}</p>
+      `;
+      movieElement.addEventListener('click', () => {
+        displayMovieDetails(movie.imdbID);
+      });
+      moviesContainer.appendChild(movieElement);
     });
-}
+  }
 
-function displayError(message) {
-    const moviesContainer = document.getElementById('movies-container');
-    moviesContainer.innerHTML = `<p>${message}</p>`;
-}
+
+
+async function displayMovieDetails(movieId) {
+    const apiKey = 'd661769b'
+    const url = `http://www.omdbapi.com/?i=${movieId}&apikey=${apiKey}`;
+    
+    try {
+      const response = await fetch(url);
+      const movie = await response.json();
+      const movieDetailsContainer = document.getElementById('detalles');
+      movieDetailsContainer.innerHTML = `
+        <button onclick="closeDetails()">Cerrar</button>
+        <h2>${movie.Title}</h2>
+        <p>Año: ${movie.Year}</p>
+        <p>Género: ${movie.Genre}</p>
+        <p>Director: ${movie.Director}</p>
+        <p>Actores: ${movie.Actors}</p>
+        <p>Sinopsis: ${movie.Plot}</p>
+        <img src="${movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'}" alt="${movie.Title}">
+      `;
+      movieDetailsContainer.style.display = 'block';
+    } catch (error) {
+      console.error("Error fetching movie details:", error);
+    }
+  }
+  
+  function closeDetails() {
+    const movieDetailsContainer = document.getElementById('detalles');
+    movieDetailsContainer.style.display = 'none';
+  }
